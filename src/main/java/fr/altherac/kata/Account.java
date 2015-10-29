a320f57b16977c720b524f817b129a9d40444607
Christophe MICHEL <christophe.michel.arolla@ext.pmu.fr>
2015-10-29 13:51:41 +0100

    Removed the balance handling code from Account and used the BalanceAwareOperationVisitor instead, since their implementation was almost identical (DRY).

--- src/main/java/fr/altherac/kata/Account.java
+++ src/main/java/fr/altherac/kata/Account.java
@@ -1,31 +1,27 @@
 package fr.altherac.kata;
 
-import fr.altherac.kata.operation.Deposit;
-import fr.altherac.kata.operation.Operations;
-import fr.altherac.kata.operation.Withdrawal;
+import fr.altherac.kata.operation.*;
 
 import java.time.LocalDate;
 
 public class Account {
 
-    private Amount balance = Amount.of(0);
-
     private final Operations operations = new Operations();
 
     public void deposit(Amount amount, LocalDate date) {
-        Deposit deposit = new Deposit(amount, date);
-        balance = deposit.applyTo(balance);
-        operations.add(deposit);
+        operations.add(new Deposit(amount, date));
     }
 
     public void withdraw(Amount amount, LocalDate date) {
-        Withdrawal withdrawal = new Withdrawal(amount, date);
-        balance = withdrawal.applyTo(balance);
-        operations.add(withdrawal);
+        operations.add(new Withdrawal(amount, date));
     }
 
     public Amount getBalance() {
-        return balance;
+        BalanceAwareOperationVisitor balanceAwareOperationVisitor = new BalanceAwareOperationVisitor();
+        for (Operation operation : operations) {
+            operation.accept(balanceAwareOperationVisitor);
+        }
+        return balanceAwareOperationVisitor.getBalance();
     }
 
     public Operations getOperations() {
